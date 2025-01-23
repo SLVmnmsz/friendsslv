1467d71f34fcc99bab61ce3d1032be4e84460659
SLVmnmsz <ignac.szilveszter@gmail.com>
2025-01-23 09:17:12 +0100

    big update
    color change, 2in1 button contact and review, added slide show

--- script.js
+++ script.js
@@ -81,59 +81,115 @@ function topFunction() {
         behavior: 'smooth'
     });
 }
- // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx facebook
- function showReviews() {
+ // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx facebook and contact
+ function showContactAndReviews() {
     const displayContent = document.getElementById('content');
+    displayContent.innerHTML = ''; // Clear previous content
 
-    // Check if the reviews div already exists
-    let fbDiv = document.getElementById('fb-reviews');
-    
-    // If it doesn't exist, create it
-    if (!fbDiv) {
-        fbDiv = document.createElement('div');
-        fbDiv.id = 'fb-reviews'; // Assign an ID for future reference
-        fbDiv.innerHTML = `
-            <div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/profile.php?id=61566203843030&amp;sk=reviews" data-tabs="reviews" data-width="500" data-height="600" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=&amp;container_width=434&amp;height=600&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61566203843030%26sk%3Dreviews&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=false&amp;small_header=true&amp;tabs=reviews&amp;width=500"><span style="vertical-align: bottom; width: 434px; height: 70px;"><iframe name="f10c0adbfe0c33588" width="500px" height="600px" data-testid="fb:page Facebook Social Plugin" title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://web.facebook.com/v10.0/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Dff54fce439a29e353%26domain%3D127.0.0.1%26is_canvas%3Dfalse%26origin%3Dhttp%253A%252F%252F127.0.0.1%253A5500%252Ff2b3fef60b879a9b2%26relation%3Dparent.parent&amp;container_width=434&amp;height=600&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61566203843030%26sk%3Dreviews&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=false&amp;small_header=true&amp;tabs=reviews&amp;width=500" style="border: none; visibility: visible; width: 434px; height: 70px;" class=""></iframe></span></div>`;
+    // Create container for all content
+    const container = document.createElement('div');
+    container.className = 'center-content';
 
-        displayContent.innerHTML = ''; // Clear previous content
-        displayContent.appendChild(fbDiv); // Append the reviews div
+    // 1. Contact Information
+    const contactDiv = document.createElement('div');
+    contactDiv.className = 'contact-info'; // Add class for styling
+    contactDiv.innerHTML = `
+        <h3>Contact Information</h3>
+        <p>Email: ignac.szilveszter@gmail.com</p>
+        <p>Phone: +36308608091</p>
+        <p>Viber: +381641061045</p>
+        <p>Social media links below</p>
+    `;
+    container.appendChild(contactDiv);
+
+    // 2. Facebook Reviews (Facebook Page Plugin)
+    const fbDiv = document.createElement('div');
+    fbDiv.innerHTML = `
+        <h3>Facebook Reviews</h3>
+        <div class="fb-page" 
+            data-href="https://www.facebook.com/profile.php?id=61566203843030&sk=reviews" 
+            data-tabs="reviews"
+            data-width="500" 
+            data-height="600" 
+            data-small-header="true" 
+            data-adapt-container-width="true" 
+            data-hide-cover="false" 
+            data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/profile.php?id=61566203843030&sk=reviews" class="fb-xfbml-parse-ignore">
+                <a href="https://www.facebook.com/profile.php?id=61566203843030&sk=reviews">Facebook Page</a>
+            </blockquote>
+        </div>
+    `;
+    container.appendChild(fbDiv);
+
+    // 3. Slideshow Section
+    const slideshowDiv = document.createElement('div');
+    slideshowDiv.id = 'slideshow';
+    slideshowDiv.innerHTML = `
+        <div class="slides">
+            <div class="slide" style="display: block;">Alen *****: nice, friendly owners. beutiful dogs. 5/5 <br> -Facebook- </div>
+            <div class="slide">We specialize in the Lagotto Romagnolo breed.</div>
+            <div class="slide">Our dogs are trained for truffle hunting and companionship.</div>
+            <div class="slide">Contact us for more information!</div>
+        </div>
+        <button id="prevBtn">Prev</button>
+        <button id="nextBtn">Next</button>
+    `;
+    container.appendChild(slideshowDiv);
+
+    displayContent.appendChild(container);
+
+    // Initialize the slideshow
+    startSlideshow();
+
+    // Load the Facebook SDK for reviews to appear correctly
+    if (window.FB) {
+        FB.XFBML.parse();
     } else {
-        // If the reviews div already exists, just clear the content to show it again
-        displayContent.innerHTML = ''; // Clear previous content
-        displayContent.appendChild(fbDiv); // Re-append to make it visible
+        loadFacebookSDK();
     }
 
-    // Load the Facebook SDK only once
-    loadFacebookSDK();
-}
+    // Function to start the slideshow
+    function startSlideshow() {
+        let slideIndex = 0;
+        const slides = document.getElementsByClassName("slide");
 
-function loadFacebookSDK() {
-    // Only load the SDK if it's not already loaded
-    if (document.getElementById('facebook-jssdk')) return;
-
-    const script = document.createElement('script');
-    script.id = 'facebook-jssdk';
-    script.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0";
-    document.body.appendChild(script);
-}
-document.querySelectorAll('.dropdown').forEach(dropdown => {
-    dropdown.addEventListener('click', function (e) {
-        e.stopPropagation(); // Prevent event bubbling
-        const dropdownContent = this.querySelector('.dropdown-content');
-
-        // Toggle visibility
-        if (dropdownContent.style.display === 'block') {
-            dropdownContent.style.display = 'none';
-        } else {
-            dropdownContent.style.display = 'block';
+        function showSlide(index) {
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  // Hide all slides
+            }
+            slides[index].style.display = "block";  // Show the current slide
         }
 
-        // Close dropdown when clicking outside
-        document.addEventListener('click', function hideDropdown(event) {
-            if (!dropdown.contains(event.target)) {
-                dropdownContent.style.display = 'none';
-                document.removeEventListener('click', hideDropdown); // Remove listener
-            }
-        });
-    });
-});
+        // Show the first slide initially
+        showSlide(slideIndex);
+
+        // Function to go to the next slide
+        document.getElementById("nextBtn").onclick = function() {
+            slideIndex = (slideIndex + 1) % slides.length;  // Loop back to the first slide
+            showSlide(slideIndex);
+        };
+
+        // Function to go to the previous slide
+        document.getElementById("prevBtn").onclick = function() {
+            slideIndex = (slideIndex - 1 + slides.length) % slides.length;  // Loop back to the last slide
+            showSlide(slideIndex);
+        };
+
+        // Change the slide automatically every 30 seconds
+        setInterval(function() {
+            slideIndex = (slideIndex + 1) % slides.length;
+            showSlide(slideIndex);
+        }, 30000);
+    }
+
+    // Load the Facebook SDK
+    function loadFacebookSDK() {
+        if (document.getElementById('facebook-jssdk')) return;
+
+        const script = document.createElement('script');
+        script.id = 'facebook-jssdk';
+        script.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0";
+        document.body.appendChild(script);
+    }
+}
